96d9867d9fe9bd140bf2e6acc871de35da93a606
hayato <116696380+haya28@users.noreply.github.com>
2024-10-17 08:58:20 +0900

    update 5 files and create 1 file

--- dist/prac01.js
+++ dist/prac01.js
@@ -1,4 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const userName = "Bob";
-console.log(`Hey, ${userName}.`);
+let deadline = new Date(2024, 10, 2, 11, 45);
+// 年、月、日、時、分を取得してフォーマット
+const year = deadline.getFullYear();
+const month = String(deadline.getMonth() + 1).padStart(2, '0'); // 月は0から始まるため +1
+const day = String(deadline.getDate()).padStart(2, '0');
+const hours = String(deadline.getHours()).padStart(2, '0');
+const minutes = String(deadline.getMinutes()).padStart(2, '0');
+// フォーマットした文字列をコンソールに出力
+console.log(`${year}/${month}/${day} ${hours}:${minutes}`);
--- src/prac01.ts
+++ src/prac01.ts
@@ -1,3 +1,10 @@
-export {}; // ファイルをモジュールとして扱いスコープを分離するために必要
-const userName: string = "Bob";
-console.log(`Hey, ${userName}.`);
+export {};
+import dayjs from "dayjs"; // dayjsのインポート
+
+const deadline: Date = new Date(2024, 9, 2, 11, 45);
+const createdAt: Date = new Date(); // 引数なしで現在日時を取得
+
+const str =
+  `期限 ${dayjs(deadline).format("YYYY/MM/DD HH:mm")}` +
+  `(登録日 ${dayjs(createdAt).format("YYYY/MM/DD HH:mm")})`;
+console.log(str);
